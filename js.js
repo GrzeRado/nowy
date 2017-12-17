9abb12542f2a24edfd759f9c8040912945bd4a15
GrzeRado <33958248+GrzeRado@users.noreply.github.com>
2017-12-17 23:10:13 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,36 @@
+var YouLikeMeat = true;
+
+if (youLikeMeat) {
+    document.write("here is the meaty menu...");
+}
+
+if (7 > 5) {
+    document.write("here is the meaty menu...");
+}
+
+if (7 < 5) {
+    document.write("here is the meaty menu...");
+}
+
+var myNum = 10;
+if (myNum > 10){
+    document.write("myNum is greater than 10");
+}
+
+var myNum = 8;
+if (myNum == 10){
+    document.write("myNum is greater than 10");
+}else{
+    document.write("myNum is not equal to 10");
+}
+
+varmyAge = 29;
+if (myAge > 30){
+    document.write("you are over 30!");
+}else if (myAge > 20){
+    document.write("you are over 20!");
+}else if (myAge > 10){
+    document.write("you are over 10!");
+}else{
+    ("you are no over 10!");
+}
