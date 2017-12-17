d687fe713f9ad142c4b098698de55576e0838ef1
GrzeRado <33958248+GrzeRado@users.noreply.github.com>
2017-12-18 00:39:11 +0100

    Add files via upload

---
+++ LOgical Oper.js
@@ -0,0 +1,9 @@
+var myAge = 25;
+
+if (myAge 18<  myAge <=30){
+    document.write("You can come, yu cool dude")
+
+} else{
+
+    document.write"you aint coming!")
+}
---
+++ Wh loop.js
@@ -0,0 +1,6 @@
+var age = 5;
+
+while(age < 10) {
+console.log ("your age is  less then 10"};
+}
+document.write("you are now over 10");
