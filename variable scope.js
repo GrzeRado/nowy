11df75fae73f1b287bfe2f4998a47cdf730c16cb
GrzeRado <33958248+GrzeRado@users.noreply.github.com>
2017-12-18 01:26:33 +0100

    Add files via upload

---
+++ variable scope.js
@@ -0,0 +1,17 @@
+getaverage = 0;
+
+
+function getAvarage (a,b){
+    var average = (a+b) /2
+    console.log(avarage);
+    return avarage;
+}
+
+var myResult = getAvarage(7,11);
+console.log("the average is" + myResult);
+
+function logResult() {
+    console.log("the average is" + myResult)
+}
+
+logResult();
