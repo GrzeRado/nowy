a299944351e2ca1ce14a208dedd79fe15669887a
GrzeRado <33958248+GrzeRado@users.noreply.github.com>
2017-12-18 02:09:19 +0100

    Add files via upload

---
+++ nan.js
@@ -0,0 +1,22 @@
+var a = "7";
+var b = 5;
+
+console.log(a+b);
+console.log(a*b);
+
+var a = "apple";
+var b = 5;
+
+console.log(a*b);
+
+if(isNan(a)){
+    console.log("that int even a number, thickie");
+} else{
+    console.log("meaning of life is " + (a*b));
+}
+
+//double negative
+if(!isNan(a)){
+    console.log("meaning of life is " + (a*b));
+
+}
