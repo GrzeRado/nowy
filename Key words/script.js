e18337cbeb7b69acedebffee88196fd64e55edbd
Grzegorz Radomski <rc36@wp.pl>
2017-12-21 00:34:46 +0100

    Constructor

--- Key words/script.js
+++ Key words/script.js
@@ -1,45 +1,19 @@
-//var myCar2 = {
+var myCar = function(maxSpeed, driver{
 
-//maxSpeed: 50,
-//driver: "Net Ninja",
-//drive: function(speed, time) {
-  //      console.log(speed * time);
-   // },
-    //test: function(){
-//    console.log(this);
-  //  }
-//};
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time) {
+        console.log(speed + time);
+    };
+ }
 
-//* var myCar3 = {
+ var myCar = new Car (70, "Ala");
+var myCar2 = new Car (70, "ola");
+var myCar3 = new Car (70, "Jas");
+var myCar4 = new Car (70, "Malgosia");
 
-   // maxSpeed: 50,
-// driver: "Shaun",
-   // drive: function(speed, time) {
-    // console.log(speed * time);
-//},
-//test: function(){
-  //  console.log(this);
-//} //
-//};
-
-//myCar2.test();
-//myCar3.test();
-//console.log(myCar2.maxSpeed);
-//myCar2.drive(30, 3); //
+myCar.drive(30, 5);
+myCar3.logDriver();
 
 
-var myCar2 = {
 
-    maxSpeed: 60,
-    driver: "Net Ninja",
-    drive: function(speed, time) {
-        console.log(speed * time);
-    },
-    logDriver: function(){
-        console.log("driver name is" + this.driver);
-    }
-};
-
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
